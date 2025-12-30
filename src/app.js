a1c9ea4422d517164105ceb962ad18afb9cca1e9
carloscostantino <carloscostantino@gmail.com>
2025-12-30 20:53:55 -0300

    probando bd

--- src/app.js
+++ src/app.js
@@ -9,8 +9,8 @@ app.get('/', (req, res) => {
 
 
 app.get('/ping', async (req, res) => {
-    const result = await pool.query('SELECT 1 + 1 as result');
-    console.log(result);
+    // const result = await pool.query('SELECT 1 + 1 as result');
+    // console.log(result);
     res.send('Enviado al usurario');
 });
 
